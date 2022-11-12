ef327a37a4164fdbcd3bc131bd3dfc46175814bd
Chananya meth <chananyameth+gitlab@gmail.com>
2022-11-12 18:37:42 +0200

    WIP: continue with the "spaces" concept

--- main.py
+++ main.py
@@ -37,21 +37,30 @@ def chords_to_line(chords, text_line: str):
     return chords_line, text_line
 
 
+def override_spaces_with_chords(chords, spaces):
+    for i in range(len(chords)):
+        spaces[i + 1] = spaces[i + 1] - len(chords[i])
+
+
 def chords_to_line_1(spaces, chords):
     line = ''
-    for i in len(chords):
-        line += ' '*spaces[i] + chords[i]
-    line += ' '*spaces[-1]
+    for i in range(len(chords)):
+        line += ' ' * spaces[i] + chords[i]
+    line += ' ' * spaces[-1]
     return line
 
 
 def space_out_chords(chords, spaces, text_line):
+    if spaces[0] == 0:
+        spaces[0] = 1
+        text_line = ' ' + text_line
+
     accumulate = 0
-    for i in len(chords):
+    for i in range(len(chords)):
         accumulate += spaces[i] + len(chords[i])
-        if diff: len(chords[i]) + 1 - spaces[i+1] > 0:
-            spaces[i+1] += diff
-            text_line = text_line[:accumulate] + '-'*diff + text_line[accumulate:]
+        if (diff := len(chords[i]) + 1 - spaces[i + 1]) > 0:
+            spaces[i + 1] += diff
+            text_line = text_line[:accumulate] + '-' * diff + text_line[accumulate:]
 
 
 def extract_chords(line: str):
@@ -64,7 +73,7 @@ def extract_chords(line: str):
     for char in line:
         if char in HEB_CHARS + OTHER_CHARS + END_LINE_CHARS:
             if chord:
-                spaces.append(counter)
+                spaces.append(counter - 1)
                 counter = 0
                 chords.append(''.join(chord))
                 chord = []
@@ -80,7 +89,7 @@ def extract_chords(line: str):
         spaces.append(counter)
         counter = 0
         chords.append(''.join(chord))
-    
+
     spaces.append(counter)
 
     return chords, spaces, ''.join(text_line)
@@ -90,12 +99,16 @@ def separate_line(line: str):
     """separate line of text into chords_line, text_line"""
     chords, spaces, text_line = extract_chords(line)
 
-    chords = reverse(chords)
-
     space_out_chords(chords, spaces, text_line)
+    override_spaces_with_chords(chords, spaces)
+
+    chords.reverse()
+    # spaces.reverse()
 
     chords_line = chords_to_line_1(spaces, chords)
     # chords_line, text_line = chords_to_line(chords, text_line)
+
+    text_line += ' ' * (len(text_line) - len(chords_line))
 
     return chords_line, text_line
 
